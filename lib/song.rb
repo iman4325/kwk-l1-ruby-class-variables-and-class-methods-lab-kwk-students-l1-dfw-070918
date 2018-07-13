52997fc63ddbb8f8f3d9dec7bae8b18b1629da4a
iman4325 <imantanzeem@hotmail.com>
2018-07-13 19:05:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,26 @@
+class Song
 
+@@genres =[]
+attr_accessor: :name, :artist, :genre
+
+@@count = 0
+
+def initialize(name,genre,artist)
+  @@count +=1
+  @@genres << genres
+  @@artists << artists
+end 
+
+def self.count
+  @@count
+end 
+def self.genres
+  @@genres = []
+end 
+def self.artists 
+  @@artists = []
+end 
+def self.genre_count 
+  genres = {}
+end 
+end   
