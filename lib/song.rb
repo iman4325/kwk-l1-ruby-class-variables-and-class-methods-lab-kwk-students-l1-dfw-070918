5a2a1a0a15faccbd22496bd696b7d0a7ffd6c9ca
iman4325 <imantanzeem@hotmail.com>
2018-07-13 19:38:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,10 +20,14 @@ def self.count
   @@count
 end 
 def self.genres
-  
+  @@genre_count = Hash.new(0)
+  @@genres.each{|key| @@genre_count[key] +=1}
+  @@genre_count 
 end 
 def self.artists 
- @@artist
+ @@artist_count = Hash.new(0)
+  @@artists.each{|key| @@artist_count[key] +=1}
+  @@artist_count 
 end 
 def self.genre_count 
   hash  = {}
